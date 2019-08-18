9470b418494a79914087df8f444be77566f8a2cc
Michael Alexander <michael_alexander_20@outlook.com>
2019-08-18 23:59:06 +0100

    Added update & delete

--- models/customer.rb
+++ models/customer.rb
@@ -21,10 +21,45 @@ class Customer
     @id = customer["id"].to_i
   end
 
+  def update()
+    sql = "UPDATE customers SET (name, funds) = ($1, $2) WHERE id = $3"
+    values = [@name, @funds, @id]
+    SqlRunner.run(sql, values)
+  end
+
+  def delete()
+    sql = "DELETE * from customers where id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
   def self.delete_all
     sql = "DELETE FROM customers"
     values = []
     SqlRunner.run(sql, values)
+  end
+
+  def films()
+    sql = "SELECT films.* FROM films INNER JOIN customers ON films.id = tickets.film_id WHERE customer_id = $1"
+    values = [@id]
+    film_data = SqlRunner.run(sql, values)
+    return Film.map_items(film_data)
+  end
+
+  def self.all()
+    sql = "SELECT * FROM customers"
+    customer_data = SqlRunner.run(sql)
+    return Customer.map_items(customer_data)
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM customers"
+    SqlRunner.run(sql)
+  end
+
+  def self.map_items(data)
+    result = data.map{|customer| Customer.new(customer)}
+    return result
   end
 
 end
--- models/ticket.rb
+++ models/ticket.rb
@@ -23,6 +23,38 @@ class Ticket
     @id = ticket["id"].to_i
   end
 
+  def update()
+    sql = "UPDATE customers SET (film_id, customer_id) = ($1, $2) WHERE id = $3"
+    values = [@film_id, @customer_id, @id]
+    SqlRunner.run(sql, values)
+  end
+
+  def delete()
+    sql = "DELETE * FROM tickets where id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+  def film()
+    sql = "SELECT * FROM films WHERE id = $1"
+    values = [@film_id]
+    film = SqlRunner.run(sql, values).first
+    return Film.new(film)
+  end
+
+  def customer()
+    sql = "SELECT * FROM customers WHERE id = $1"
+    values = [@customer_id]
+    customer = SqlRunner.run(sql, values).first
+    return Customer.new(star)
+  end
+
+  def self.all()
+    sql = "SELECT * FROM tickets"
+    data = SqlRunner.run(sql)
+    return data.map{|ticket| Tickets.new(ticket)}
+  end
+
   def self.delete_all
     sql = "DELETE FROM tickets"
     values = []
